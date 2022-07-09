fbf8606b7ad54f1433860737a262b11da505dcb4
Raul <jimenezraul1981@gmail.com>
2022-07-08 22:28:33 -0400

    fixing issue in gallery in production

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -9,14 +9,14 @@ const Gallery = () => {
       setGallery(data);
     });
   }, []);
-
+  
   return (
     <div className='flex flex-wrap justify-center w-full p-3 container mx-auto'>
       <div className='flex flex-wrap  mb-2 md:mb-0 w-full bg-gray-700 text-gray-300 rounded-lg shadow-lg border border-gray-700'>
         <div className='mb-2 p-2 rounded-t-lg w-full text-center font-bold text-2xl bg-gray-800'>
           <h1>Gallery</h1>
         </div>
-        {gallery?.map((image) => (
+        {gallery.length > 0 && gallery.map((image) => (
           <div key={image.name} className='p-2 w-1/2 md:w-1/3 lg:w-1/4'>
             <img className='rounded-lg' src={image.url} alt='' />
           </div>
