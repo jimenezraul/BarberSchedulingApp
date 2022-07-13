6c65a18703a53285a1d24b5d1112337cc95e9c10
Raul <jimenezraul1981@gmail.com>
2022-07-13 11:42:33 -0400

    fixed issue with the menu bar not closing when clicking the x

--- client/src/components/NavBar/hook/index.js
+++ client/src/components/NavBar/hook/index.js
@@ -9,12 +9,22 @@ export function useOutside(ref, setIsOpen) {
      * Alert if clicked on outside of element
      */
     function handleClickOutside(event) {
+      // if the target is a image, then close it
+      if (event.target.tagName === "IMG") {
+        setIsOpen(false);
+        return;
+      }
+
       if (ref.current && !ref.current.contains(event.target)) {
         setIsOpen(false);
+        return;
       }
     }
     // Bind the event listener
     document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("touchstart", handleClickOutside);
+    // on mobile scroll
+    window.addEventListener("scroll", handleClickOutside);
     return () => {
       // Unbind the event listener on clean up
       document.removeEventListener("mousedown", handleClickOutside);
